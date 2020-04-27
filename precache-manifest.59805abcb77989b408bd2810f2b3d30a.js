d4d4cbfb1b49ae7af0bdcb001ff582c5bd848980
Marudhupandian Ramaiah <rv.marudhubtech@gmail.com>
2020-04-28 00:21:02 +0530

    Updates

---
+++ precache-manifest.59805abcb77989b408bd2810f2b3d30a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb23991abf88edec0fb0052d719fcd16",
+    "url": "/marudhu/index.html"
+  },
+  {
+    "revision": "64169f62e56cbef3e9c7",
+    "url": "/marudhu/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4ac47463ce83888c9d40",
+    "url": "/marudhu/static/js/2.ef911d8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marudhu/static/js/2.ef911d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64169f62e56cbef3e9c7",
+    "url": "/marudhu/static/js/main.bd1f3d07.chunk.js"
+  },
+  {
+    "revision": "9ceefc216153e0f44860",
+    "url": "/marudhu/static/js/runtime-main.eedb1d28.js"
+  }
+]);
